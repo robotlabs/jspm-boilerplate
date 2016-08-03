0dfd35c70b1dae939f36b20c63d78db8345fb657
Roberto Cascavilla <snaptobrain@gmail.com>
2016-08-03 13:42:08 +0100

    removed unused code. updated build process

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,14 +1,6 @@
-console.log('>>ciao');
 import {bootstrap} from './mod-1.js';
-// console.log(window.System);
-// System.import('./main.js');
-var test = 9;
-let test2 = 10;
-var x = test + test2;
-console.log(x);
 fuck();
 function fuck() {
-    console.log('fuck!');
-
+    console.log('+++++fuck!');
     bootstrap();
 }
